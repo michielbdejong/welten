1dfee768111eaf742fef1fd93b02cad261059d19
Michiel de Jong <michiel@unhosted.org>
2013-09-19 22:24:33 +0300

    attempt to complete remotestorage-server with Content-Type and perms

--- remotestorage-server.js
+++ remotestorage-server.js
@@ -1,17 +1,37 @@
 var fs = require('fs'),
     url=require('url'),
     crypto=require('crypto'),
+    xattr=require('xattr-async'),
     dataDir;//to be set when requiring this module
 
+function doWrite(path, contentType, content, cb) {
+  fs.writeFile(path, content, function(err) {
+    if(err) {
+      cb(err);
+    } else {
+      xattr.set(path, 'Content-Type', contentType, cb);
+    }
+  });
+}
+
+function makePath(str) {
+  return './data/'+str;//FIXME use proper protection against relative '../' paths
+}
+
 function makeScopePaths(scopes) {
-  var scopePaths=[];
+  var scopePaths={};
   for(var i=0; i<scopes.length; i++) {
     var thisScopeParts = scopes[i].split(':');
+    if((thisScopeParts.length!=2)
+        || (thisScopeParts[0].indexOf('/')!=-1)
+        || (['r', 'rw'].indexOf(thisScopeParts[1])==-1)) {
+      continue;
+    }
     if(thisScopeParts[0]=='root') {
-      scopePaths.push('/:'+thisScopeParts[1]);
+      scopePaths[makePath('')]=thisScopeParts[1];
     } else {
-      scopePaths.push('/'+thisScopeParts[0]+'/:'+thisScopeParts[1]);
-      scopePaths.push('/public/'+thisScopeParts[0]+'/:'+thisScopeParts[1]);
+      scopePaths[makePath(thisScopeParts[0]+'/']=thisScopeParts[1];
+      scopePaths[makePath('public/'+thisScopeParts[0]+'/']=thisScopeParts[1];
     }
   }
   return scopePaths;
@@ -21,10 +41,63 @@ function createToken(scopes, cb) {
   crypto.randomBytes(48, function(ex, buf) {
     var token = buf.toString('hex');
     var scopePaths = makeScopePaths(scopes);
-    doWrite('/apps/'+token, JSON.stringify(scopePaths), function(err) {
+    doWrite('/apps/'+token, 'application/json', JSON.stringify(scopePaths), function(err) {
       cb(token);
     });
   });
+}
+
+function perms(authorizationHeader, path, origin, isGet, res) {
+  var pubPath = makePath('public/');
+  if(isGet && path.substr(-1) != '/' && path.substr(0, pubPath.length)==pubPath) {
+    return false;
+  }
+  var scopePaths, token = authorizationHeader.substring('Bearer '.length);
+  try {
+    scopePaths = JSON.parse(fs.readFileSync('/apps/'+token));
+  } catch(e) {//Bearer token unknown
+    respondStatus(res, 401, origin);
+    return true;
+  }
+  for(var i in scopePaths) {
+    if((path.substring(i.length)==i)
+        && (isGet || scopePaths[i]=='rw')) {
+      return false;
+    }
+  }
+  //Bearer token gives no access
+  respondStatus(res, 402, origin);
+  return true;
+}
+
+function getCurrEtag(path) {
+  var stat;
+  try {
+    stat = fs.statSync(path);
+  } catch(e) {//probably a 404
+    return false;
+  }
+  return stat.mtime.getTime().toString();
+}
+
+function ifMatch(etag, path, origin, isGet, res) {
+  if(getCurrEtag(path)!=etag) {
+    respondStatus(res, (isGet?304:412), origin, currEtag);
+    return true;
+  }
+}
+
+function ifNoneMatch(etags, path, origin, isGet, res) {
+  if(!Array.isArray(etags)) {
+    etags = [etags];
+  }
+  var currEtag = getCurrEtag(path);
+  for(var i=0; i<etags.length; i++) {
+    if(currEtag==etags[i]) {
+      respondStatus(res, (isGet?304:412), origin, currEtag);
+      return true;
+    } 
+  }
 }
 
 function writeHead(res, status, origin, etag, contentType) {
@@ -57,6 +130,17 @@ function respondStatus(res, status, origin, etag) {
   res.end();
 }
 
+function serveGetDir(path, origin, res) {
+  fs.readdir(path, function(err2, listing) {
+    var i, etags={}, thisStat;
+    for(i=0; i<listing.length; i++) {
+      thisStat = fs.statSync(path+listing[i]);
+      etags[listing[i] + (thisStat.isDirectory()?'/':'')] = thisStat.mtime.getTime().toString();
+    }
+    respondContent(res, JSON.stringify(etags), origin, stat.mtime.getTime().toString(), 'application/json');
+  });
+}
+
 function serveGet(path, origin, res) {
   fs.stat(path, function(err1, stat) {
     if(err1) {
@@ -64,17 +148,9 @@ function serveGet(path, origin, res) {
       respondStatus(res, 404, origin);
     } else {
       if(path.substr(-1)=='/' && stat.isDirectory()) {
-        fs.readdir(path, function(err2, listing) {
-          var i, etags={}, thisStat;
-          for(i=0; i<listing.length; i++) {
-            thisStat = fs.statSync(path+listing[i]);
-            etags[listing[i] + (thisStat.isDirectory()?'/':'')] = thisStat.mtime.getTime().toString();
-          }
-          respondContent(res, JSON.stringify(etags), origin, stat.mtime.getTime().toString(), 'application/json');
-        });
+        serveGetDir(path, origin, res);
       } else if(path.substr(-1)!='/' && stat.isFile()) {
         fs.readFile(path, function(err2, data) {
-          //console.log(path, err1, err2, data);
           respondContent(res, data, origin, stat.mtime.getTime().toString(), 'application/octet-stream');
         });
       } else {
@@ -91,8 +167,8 @@ function serveDelete(path, origin, res) {
   });
 }
 
-function servePut(path, content, origin, res) {
-  fs.writeFile(path, content, function(err) {
+function servePut(path, contentType, content, origin, res) {
+  doWrite(path, contentType, content, function(err) {
     respondJson(res, (err?500:200), null, origin);
   });
 }
@@ -101,33 +177,13 @@ function serveOptions(origin, res) {
   respondJson(res, null, origin);
 }
 
-function makePath(str) {
-  return './data/'+str;
-}
-
-function ifNoneMatch(etags, path, origin, isGet, res) {
-  var stat;
-  try {
-    stat = fs.statSync(path);
-  } catch(e) {//probably a 404
-    return false;
-  }
-  var currEtag = stat.mtime.getTime().toString();
-  for(var i=0; i<etags.length; i++) {
-    if(currEtag==etags[i]) {
-      respondStatus(res, (isGet?304:412), origin, currEtag);
-      return true;
-    } 
-  }
-}
-
 function serve(req, res) {
   var path = makePath(url.parse(req.url, true).pathname),
     origin = req.headers.origin;
   if(req.method=='OPTIONS') { serveOptions(origin, res); return; }
-//  if(perms(req.headers.authorization, path, origin, (req.method=='GET'), res)) { return; }
+  if(perms(req.headers.authorization, path, origin, (req.method=='GET'), res)) { return; }
   if(ifNoneMatch([req.headers['if-none-match']], path, origin, (req.method=='GET'), res)) { return; }
-//  if(ifMatch(req.headers['if-match'], path, origin,(req.method=='GET'), res)) { return; }
+  if(ifMatch(req.headers['if-match'], path, origin,(req.method=='GET'), res)) { return; }
   if(req.method=='GET') { serveGet(path, origin, res); return; }
   if(req.method=='DELETE') { serveDelete(path, origin, res); return; }
   if(req.method=='PUT') {
@@ -136,7 +192,7 @@ function serve(req, res) {
       dataStr+=chunk;
     });
     req.on('end', function() {
-      servePut(path, dataStr, origin, res);
+      servePut(path, req.headers['content-type'], dataStr, origin, res);
     });
     return; 
   }
