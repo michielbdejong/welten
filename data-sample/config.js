94926b1600e01de96d86c48827d0416396c87c98
root <michiel@unhosted.org>
2013-09-20 15:56:43 +0200

    load platforms only if their config exists

--- data-sample/config.js
+++ data-sample/config.js
@@ -1,69 +1,72 @@
-var fs = require('fs');
-module.exports = {
-  dispatcher: {
-    port: 4272,
-    token: '...',
-    //this will cause it to open a secure WebSocket (wss://), leave it out to get a standard/insecure one (ws://):
-    https: {
-      key: fs.readFileSync('./data/tls/tls.key'), 
-      cert: fs.readFileSync('./data/tls/tls.cert'), 
-      ca: fs.readFileSync('./data/tls/chain.pem')
-    }
-  },
-  facebook: {
-    access_token: '...'
-  },
-  google: {
-    client_id: '...',
-    client_secret: '...',
-    redirect_uri: '...',
-    access_token: '...',//obtain from OAuth2 code using 'allow' button in August app, and then 'set' verb
-  },
-  twitter: {
-    '@handle': {
-      consumer_key: '...',
-      consumer_secret: '...',
-      access_token_key: '...',
-      access_token_secret: '...'
-    }
-  },
-  irc: {
-    host: 'irc.freenode.net',
-    nick: '...',
-    channels: ['#welten']
-  },
-  email: {
-    sendgrid: {
-      usr: '...',
-      pwd: '...',
-      from: '...@...',
-      fromname: '... ...',
+//var fs = require('fs');
+module.exports = function(dataDir) {
+  return {
+    dispatcher: {
+      port: 4272,
+      token: '...',
+      //this will cause it to open a secure WebSocket (wss://), leave it out to get a standard/insecure one (ws://):
+//      https: {
+//        key: fs.readFileSync(dataDir+'tls/tls.key'), 
+//        cert: fs.readFileSync(dataDir+'tls/tls.cert'), 
+//        ca: fs.readFileSync(dataDir+'tls/chain.pem'),
+//      },
     },
-    imap: {
-      user: '...',
+//    facebook: {
+//      access_token: '...',
+//    },
+//    google: {
+//      client_id: '...',
+//      client_secret: '...',
+//      redirect_uri: '...',
+//      access_token: '...',//obtain from OAuth2 code using 'allow' button in August app, and then 'set' verb
+//    },
+//    twitter: {
+//      '@handle': {
+//        consumer_key: '...',
+//        consumer_secret: '...',
+//        access_token_key: '...',
+//        access_token_secret: '...',
+//      },
+//    },
+//    irc: {
+//      host: 'irc.freenode.net',
+//      nick: '...',
+//      channels: ['#welten'],
+//    },
+//    email: {
+//      sendgrid: {
+//        usr: '...',
+//        pwd: '...',
+//        from: '...@...',
+//        fromname: '... ...',
+//      },
+//      imap: {
+//        user: '...',
+//        password: '...',
+//        host: '...',
+//        port: 993,
+//        tls: true,
+//        tlsOptions: { rejectUnauthorized: false },
+//      }
+//    },
+    remotestorage: {
+//      https: {
+//        key: fs.readFileSync(dataDir+'tls/tls.key'),
+//        cert: fs.readFileSync(dataDir+'tls/tls.cert'),
+//        ca: fs.readFileSync(dataDir+'tls/chain.pem'),
+//      },
+      port: 8012,
       password: '...',
-      host: '...',
-      port: 993,
-      tls: true,
-      tlsOptions: { rejectUnauthorized: false }
-    }
-  },
-  remotestorage: {
-    https: {
-      key: fs.readFileSync('./data/tls/tls.key'),
-      cert: fs.readFileSync('./data/tls/tls.cert'),
-      ca: fs.readFileSync('./data/tls/chain.pem')
+      dataRoot: dataDir,
     },
-    port: 8012,
-    password: '...'
-  },
-  www: {
-    contentDir: './data/public_html',
-    //this will cause it to listen on port 443 as well, leave it out for just port 80:
-    https: {
-      key: fs.readFileSync('./data/tls/tls.key'),
-      cert: fs.readFileSync('./data/tls/tls.cert'),
-      ca: fs.readFileSync('./data/tls/chain.pem')
-    }
-  }
+    www: {
+      contentDir: dataDir+'public_html/',
+      portOffset: 7000, //will result in ports 7080 (and 7443) instead of 80 (and 443)
+//      https: {
+//        key: fs.readFileSync(dataDir+'tls/tls.key'),
+//        cert: fs.readFileSync(dataDir+'tls/tls.cert'),
+//        ca: fs.readFileSync(dataDir+'tls/chain.pem')
+//      },
+    },
+  };
 };
--- welten.js
+++ welten.js
@@ -1,90 +1,98 @@
-var sockjs = require('sockjs'),
-    fs = require('fs'),
-    mkdirp = require('mkdirp'),
-    config = require('./data/config').dispatcher,
-    platforms = {};
+if(process.argv.length != 3) {
+  console.log('Usage: node welten.js path/to/data/dir');
+  process.exit(1);
+}
 
-//require all platforms from `lib/`:
-fs.readdir('./lib/', function(err, data) {
-  var i, platform, clients=[];
-  if(!err) {
-    for(i=0; i<data.length; i++) {
-      platform =data[i].substring(0, data[i].length-3);
-      platforms[platform]=require('./lib/'+platform);
-      if(typeof(platforms[platform].on)=='function') {
-        platforms[platform].on('message', (function(p) {
-          return function(msg) {
-            if(msg.timestamp) {
-              var dir = './data/inbox/'+p+'/'
-                +msg.timestamp.toString().substring(0,4)+'/'
-                +msg.timestamp.toString().substring(4,7)+'/';
-              mkdirp(dir, function(err) {
-                if(!err) {
-                  fs.writeFile(dir+msg.timestamp.toString().substring(7), JSON.stringify(msg));
-                }
-              });   
-            }
-            for(var j=0;j<clients.length;j++) {
-              if(clients[j] && typeof(clients[j].write)=='function') {
-                clients[j].write(JSON.stringify({platform: p, type: 'incoming', message: msg}));
+var fs = require('fs'),
+  sockjs = require('sockjs'),
+  mkdirp = require('mkdirp'),
+  dataDir = process.argv[2],
+  config, platforms = {}, clients = [];
+
+if(dataDir.substr(-1)!='/') { dataDir += '/'; }
+try {
+  config = require(dataDir+'config.js')(dataDir);
+} catch (e) {
+  console.log('Please copy ./data-sample/ to '+dataDir+' to get started');
+  process.exit(1);
+}
+
+// load platforms:
+for(var i in config) {
+  if(config[i] && i != 'dispatcher') {
+    platforms[i]=require('./lib/'+i)(config[i]);
+    if(typeof(platforms[i].on)=='function') {
+      platforms[platform].on('message', (function(p) {
+        return function(msg) {
+          if(msg.timestamp) {
+            var dir = './data/inbox/'+p+'/'
+              +msg.timestamp.toString().substring(0,4)+'/'
+              +msg.timestamp.toString().substring(4,7)+'/';
+            mkdirp(dir, function(err) {
+              if(!err) {
+                fs.writeFile(dir+msg.timestamp.toString().substring(7), JSON.stringify(msg));
               }
+            });   
+          }
+          for(var j=0;j<clients.length;j++) {
+            if(clients[j] && typeof(clients[j].write)=='function') {
+              clients[j].write(JSON.stringify({platform: p, type: 'incoming', message: msg}));
             }
-          };
-        })(platform));
-      }
-      console.log('added platform: '+platform);
+          }
+        };
+      })(platform));
     }
+    console.log('added platform: '+i);
   }
-  
-  //the core function that dispatches a command to a platform:
-  function dispatch(obj, cb) {
-    console.log('dispatching', obj);
-    if((typeof(obj) != 'object') || (typeof(obj.target) != 'string')) {
-      cb({error: 'obj.target should be a string', object: obj});
-    } else {
-      var platformAndTarget = obj.target.split(':');
-      if((obj.token == config.token) && (platforms[platformAndTarget[0]]) && (platforms[platformAndTarget[0]][obj.verb]))  {
-        platforms[platformAndTarget[0]][obj.verb](obj.object, platformAndTarget[1], cb);
-      } else {
-        cb({error: 'cannot dispatch that', object: obj});
-      }
-    }
-  }
-
-  //set up webserver to bind WebSocket server to:
-  var webServer;
-  function serve(req, res) {
-    res.writeHead(200);
-    res.end('Welcome to sockethub welten. please connect a WebSocket to this port');
-  }
-  if(config.https) {
-    webServer = require('https').createServer(config.https, serve);
+}
+ 
+function dispatchCommand(obj, cb) {
+  console.log('dispatching', obj);
+  if((typeof(obj) != 'object') || (typeof(obj.target) != 'string')) {
+    cb({error: 'obj.target should be a string', object: obj});
   } else {
-    webServer = require('http').createServer(serve);
+    var platformAndTarget = obj.target.split(':');
+    if((obj.token == config.dispatcher.token) && (platforms[platformAndTarget[0]]) && (platforms[platformAndTarget[0]][obj.verb]))  {
+      platforms[platformAndTarget[0]][obj.verb](obj.object, platformAndTarget[1], cb);
+    } else {
+      cb({error: 'cannot dispatch that', object: obj});
+    }
   }
-  webServer.listen(config.port);
+}
 
-  //set up WebSocket server to receive commands and call the `dispatch` function:
-  var sockServer = sockjs.createServer();
-  sockServer.on('connection', function(conn) {
-    console.log('connected!');
-    clients.push(conn);
-    conn.on('data', function(chunk) {
-      var obj;
-      try { obj = JSON.parse(chunk); } catch(e) {}
-      if(typeof(obj) == 'object') {
-        dispatch(obj, function(res) {
-          console.log('writing back', res);
-          conn.write(JSON.stringify(res));
-        });
-      } else {
-        conn.write('what you sent is not JSON: '+chunk);
-      }
-    });
+//set up webserver to bind WebSocket server to:
+var webServer;
+function serve(req, res) {
+  res.writeHead(200);
+  res.end('Welcome to sockethub welten. please connect a WebSocket to this port');
+}
+if(config.dispatcher.https) {
+  webServer = require('https').createServer(config.dispatcher.https, serve);
+} else {
+  webServer = require('http').createServer(serve);
+}
+webServer.listen(config.dispatcher.port);
+
+//set up WebSocket server to receive commands and call the `dispatch` function:
+var sockServer = sockjs.createServer();
+sockServer.on('connection', function(conn) {
+  console.log('connected!');
+  clients.push(conn);
+  conn.on('data', function(chunk) {
+    var obj;
+    try { obj = JSON.parse(chunk); } catch(e) {}
+    if(typeof(obj) == 'object') {
+      dispatchComand(obj, function(res) {
+        console.log('writing back', res);
+        conn.write(JSON.stringify(res));
+      });
+    } else {
+      conn.write('what you sent is not JSON: '+chunk);
+    }
   });
-  sockServer.installHandlers(webServer, { prefix: '/sock' });
-
-  console.log('Test if '
-      +'http'+(config.https?'s':'')+'://localhost:'+config.port+'/ works, then point your unhosted web app to '
-      +'ws'+(config.https?'s':'')+'://localhost:'+config.port+'/sock/websocket - your token is: '+config.token);
 });
+sockServer.installHandlers(webServer, { prefix: '/sock' });
+
+console.log('Test if '
+  +'http'+(config.dispatcher.https?'s':'')+'://localhost:'+config.dispatcher.port+'/ works, then point your unhosted web app to '
+  +'ws'+(config.dispatcher.https?'s':'')+'://localhost:'+config.dispatcher.port+'/sock/websocket - your token is: '+config.dispatcher.token);
